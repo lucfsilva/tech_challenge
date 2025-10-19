8d3aa5fe14fff33290ffbc4ec8784a90de6c5292
Lucas <lucfsilva@gmail.com>
2025-10-19 01:22:38 -0300

    Final

--- fase_1/src/main.py
+++ fase_1/src/main.py
@@ -12,8 +12,8 @@ print('Início do Tech Challenge - Fase 1')
 print('Diagnóstico de diabetes')
 
 dados = exploracao.carregar('mathchi/diabetes-data-set')
-# exploracao.analise_descritiva(dados)
-# exploracao.analise_grafica(dados)
+exploracao.analise_descritiva(dados)
+exploracao.analise_grafica(dados)
 
 dados = pre_processamento.limpar(dados)
 
@@ -24,12 +24,12 @@ X_treino, X_teste, y_treino, y_teste = train_test_split(X_dados, y_dados, test_s
 X_treino_escalonado, X_teste_escalonado = pre_processamento.escalonar(X_treino, X_teste)
 # X_treino_balanceado, y_treino_balanceado = pre_processamento.balancear(X_treino_escalonado, y_treino) # Melhora o resultado de Regressão Logística e KNN, mas não o suficiente para superar a árvore de descisão.
 
-# X_total = pd.concat([X_treino_balanceado, X_teste_escalonado], axis=0)
-# y_total = pd.concat([y_treino_balanceado, y_teste], axis=0)
-# dados = X_total.copy()
-# dados['Outcome'] = y_total.values
-# exploracao.analise_descritiva(dados)
-# exploracao.analise_grafica(dados)
+X_total = pd.concat([X_treino_escalonado, X_teste_escalonado], axis=0)
+y_total = pd.concat([y_treino, y_teste], axis=0)
+dados = X_total.copy()
+dados['Outcome'] = y_total.values
+exploracao.analise_descritiva(dados)
+exploracao.analise_grafica(dados)
 
 # Treino e avaliação dos modelos
 modelos = {
@@ -56,8 +56,8 @@ modelos = {
     #     random_state=42)
 }
 
-melhor_modelo = modelagem.avaliar(modelos, X_treino_escalonado, y_treino, X_teste_escalonado, y_teste)
-# melhor_modelo = modelagem.avaliar(modelos, X_treino_balanceado, y_treino_balanceado, X_teste_escalonado, y_teste)
+melhor_modelo = modelagem.avaliar(modelos, X_treino_escalonado, y_treino, X_teste_escalonado, y_teste) # Sem balanceamento
+# melhor_modelo = modelagem.avaliar(modelos, X_treino_balanceado, y_treino_balanceado, X_teste_escalonado, y_teste) # Com balanceamento
 
 # Ainda é preciso interpretar os resultados usando feature importance e SHAP.
 
